2fd3bb84ea413d37309cb610820492544fc49de7
Bijaya Limbu <bl18aat@herts.ac.uk>
2020-01-23 22:01:14 +0000

    Name Mixer

---
+++ js/main.js
@@ -0,0 +1,41 @@
+new Vue({
+    el:'#app',
+    data:{
+        state:true,
+        inputName:'',
+        names:[],
+        showError: false,
+        result:''
+    },
+    methods:{
+        addNameToList(){
+            console.log(this.names)
+            if(this.validate(this.inputName)){
+                this.names.push(this.inputName);
+                this.inputName='';
+                this.showError = false;
+            } else {
+                this.showError = true;
+            }
+        },
+        validate(value){
+            if(value !== ''){
+                return true
+            }else{
+                return false
+            }
+
+        },
+        showResults(){
+            let rand = this.names[Math.floor(Math.random() * this.names.length)]
+            this.result = rand
+            this.state=false
+        },
+        resetApp(){
+            this.state = true;
+            this.name = [];
+            this.result = ''
+        }
+    }
+
+})
